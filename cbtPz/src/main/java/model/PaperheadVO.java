68ab046ceb173a77aeee79527e0f0ab186dba0c8
admin <tnehdtlrl@naver.com>
2020-09-16 18:39:15 +0900

    0916양수동

--- cbtPz/src/main/java/model/PaperheadVO.java
+++ cbtPz/src/main/java/model/PaperheadVO.java
@@ -4,7 +4,7 @@ import lombok.Data;
 
 @Data
 public class PaperheadVO {
-	private String paperhead_id;
+	private String paperhead_id; 
 	private String paper_type_cd;
 	private String paper_round;
 }
