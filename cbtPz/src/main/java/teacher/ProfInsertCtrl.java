e09433abd49def47478210075012f3f99e851ddc
hanmin7 <minyo3746@gmail.com>
2020-09-17 14:43:35 +0900

    0917_p

--- cbtPz/src/main/java/teacher/ProfInsertCtrl.java
+++ cbtPz/src/main/java/teacher/ProfInsertCtrl.java
@@ -6,14 +6,29 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.beanutils.BeanUtils;
+
 import controller.Controller;
+import model.TeacherDAO;
+import model.TeacherVO;
 
 public class ProfInsertCtrl implements Controller {
 
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("교사회원등록");
 		
+		TeacherVO teacher = new TeacherVO();
 		
+		try {
+			BeanUtils.copyProperties(teacher, request.getParameterMap());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		TeacherDAO.getInstance().insert(teacher);
+		
+		request.getRequestDispatcher("/member/login.jsp").forward(request, response);
 		
 	}
 
