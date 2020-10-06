fbe529426fffd8bbaaeb169dde841fa756409329
admin <tnehdtlrl@naver.com>
2020-10-06 15:15:30 +0900

    10_06 프로시져 완료

--- cbtPz/src/main/java/study/ProblemSubCtrl.java
+++ cbtPz/src/main/java/study/ProblemSubCtrl.java
@@ -9,8 +9,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import controller.Controller;
+import model.MemberVo;
 import model.PaperHeadDAO;
-import model.ProblemVO;
+import model.SearchVO;
 
 public class ProblemSubCtrl implements Controller {
 	/**과목별 문제 불러오기**/
@@ -18,17 +19,26 @@ public class ProblemSubCtrl implements Controller {
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String path = "studyPaper.jsp";
 		
-		ProblemVO subVO = new ProblemVO();
+		MemberVo memberVo= (MemberVo) request.getSession().getAttribute("login");
+		SearchVO searchVO = new SearchVO();
 		
+		String member_id = memberVo.getMember_id();
 		String subject = request.getParameter("subject");
 		
-		subVO.setSubject(subject);
 		
-		PaperHeadDAO dao = new PaperHeadDAO();
 		
-		List<Map<String, Object>> sub = dao.selectSubjectType(subVO);
+		//VO에 담기		
+		searchVO.setMember_id(member_id);
+		searchVO.setSubject(subject);
 		
-		request.getSession().setAttribute("problemList", sub);
+		//문제등록 				
+		int next = PaperHeadDAO.getInstance().insert_Proc(searchVO);
+		
+		//문제조회
+		searchVO.setSolve_id(Integer.toString(next)); 
+		List<Map<String, Object>> selectproblem = PaperHeadDAO.getInstance().selectAllType(searchVO);
+
+		request.getSession().setAttribute("problemList", selectproblem);
 		
 		request.getRequestDispatcher("/study/"+path).forward(request, response);
 	}
