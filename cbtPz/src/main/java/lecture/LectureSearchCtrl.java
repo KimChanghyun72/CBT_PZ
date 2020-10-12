5e9a1dbd1141990441dd918bc56bf0382397c8d9
hanmin7 <minyo3746@gmail.com>
2020-10-12 09:04:43 +0900

    1012_

---
+++ cbtPz/src/main/java/lecture/LectureSearchCtrl.java
@@ -0,0 +1,19 @@
+package lecture;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import controller.Controller;
+
+public class LectureSearchCtrl implements Controller {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
